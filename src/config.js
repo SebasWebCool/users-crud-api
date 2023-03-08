61eea50efae143867fd02cf72b6ca8cf68cceebf
Sebas <sebasweb2000@gmail.com>
2023-03-08 17:37:22 -0400

    fix

--- src/config.js
+++ src/config.js
@@ -2,14 +2,15 @@
 require('dotenv').config()
 
 const config = {
-    port: process.env.DB_PORT || 9000,
+    port: process.env.PORT || 9000,
     nodeEnv: process.env.NODE_ENV || 'development', //? Desarrollo, Testing, Produccion
     jwtSecret: process.env.JWT_SECRET,
     db: {
         host: process.env.DB_HOST ,
         username: process.env.DB_USER ,
         password: process.env.DB_PASS ,
-        dbName: process.env.DB_NAME
+        dbName: process.env.DB_NAME,
+        dbPort: process.env.DB_PORT
     }
 
 }

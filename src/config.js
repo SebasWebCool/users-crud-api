5908ae226c8b158d7997d6704f2efe912e8fb4c7
Sebasweb <sebasweb2000@gmail.com>
2023-09-07 18:01:50 -0400

    added new config

--- src/config.js
+++ src/config.js
@@ -5,13 +5,13 @@ require('dotenv').config()
 const config = {
     port: process.env.PORT || 9000,
     nodeEnv: process.env.NODE_ENV || 'development', //? Desarrollo, Testing, Produccion
-    jwtSecret: process.env.JWT_SECRET,
+    jwtSecret: process.env.JWT_SECRET || 'secret',
     db: {
-        host: process.env.DB_HOST ,
-        username: process.env.DB_USER ,
-        password: process.env.DB_PASS ,
-        dbName: process.env.DB_NAME,
-        dbPort: process.env.DB_PORT
+        host: process.env.HOSTNAME ,
+        username: process.env.USERNAME ,
+        password: process.env.PASSWORD ,
+        dbName: process.env.DATABASE,
+        dbPort: process.env.PORT
     }
 
 }
